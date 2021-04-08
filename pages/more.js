e69602c6d2a6469c27f06fc61d8a17bb45276766
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-04-08 17:13:37 +0300

    Update more.js

--- pages/more.js
+++ pages/more.js
@@ -1,11 +1,12 @@
 import Layout from "../components/layout";
+import ThemeSelect from "../components/theme-select";
 
 
 export default function More() {
 
     return (
         <Layout>
-            more
+            <ThemeSelect/>
         </Layout>
     )
 }
