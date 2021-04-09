4e1d864d5915c73543e2841c208faa27180a39c5
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-04-09 11:26:42 +0300

    fixed className

--- components/stack/stack.js
+++ components/stack/stack.js
@@ -2,11 +2,11 @@ import React from "react";
 import style from './stack.module.css';
 import cn from 'classnames';
 
-const Stack = ({gap = 10, column = false, children}) => {
+const Stack = ({gap = 10, column = false, className, children}) => {
     return (
         <div
             style={{"--gap" : `${gap}px`}}
-            className={cn(style.stack, column && style.column)}>
+            className={cn(style.stack, column && style.column, className)}>
             {children}
         </div>
 
