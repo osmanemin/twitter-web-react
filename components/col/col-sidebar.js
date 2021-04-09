c8d96399c66372c9c57dd32e692adba1603c36be
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-04-09 11:27:25 +0300

    added popup

--- components/col/col-sidebar.js
+++ components/col/col-sidebar.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import style from './col-sidebar.module.css';
 import cn from 'classnames';
 import Navigation from "../navigation/navigation";
 import ThemeButton from "../theme-button/theme-button";
 import ProfileBox from "../profile-box/profile-box";
+import TweetPopup from "../tweet-popup/tweet-popup";
+import {Tweet} from '../icons'
 
 const ColSidebar = ({flat}) => {
+
+    const [showPopup, setShowPopup] = useState(false);
+
     return (
         <div className={cn(style.sidebar)}>
             <Navigation flat={flat}/>
+
             <div className={style.tweet}>
-                <ThemeButton big full={!flat}>{flat ? 'a' : "Tweet"}</ThemeButton>
+                <ThemeButton onClick={() => {setShowPopup(true)}} big full={!flat}>{flat ? <Tweet/> : "Tweet"}</ThemeButton>
             </div>
+
+            {showPopup && <TweetPopup setShowPopup={setShowPopup}/>}
+
             <div className={style.profile}>
                 <ProfileBox flat={flat}/>
             </div>
